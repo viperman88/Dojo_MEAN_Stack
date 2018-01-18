f4a0fc9d270665260ba77c419913f713152f1a61
Robert Amato <robertamato@Roberts-MBP.fios-router.home>
2018-01-18 15:18:05 -0500

    complete

--- Node/cars_cats/app.js
+++ Node/cars_cats/app.js
@@ -8,7 +8,7 @@ var server = http.createServer(function(request, response) {
     console.log('client request URL: ', request.url);
     // this is how we do routing:
     if (request.url === '/') {
-        fs.readFile('index.html', 'utf8', function(errors, contents) {
+        fs.readFile('views/index.html', 'utf8', function(errors, contents) {
             response.writeHead(200, {
                 'Content-Type': 'text/html'
             });
@@ -16,7 +16,7 @@ var server = http.createServer(function(request, response) {
             response.end();
         });
     } else if (request.url === "/cars") {
-        fs.readFile('cars.html', 'utf8', function(errors, contents) {
+        fs.readFile('views/cars.html', 'utf8', function(errors, contents) {
             response.writeHead(200, {
                 'Content-type': 'text/html'
             });
@@ -24,7 +24,7 @@ var server = http.createServer(function(request, response) {
             response.end();
         });
     } else if (request.url === "/cats") {
-        fs.readFile('cats.html', 'utf8', function(errors, contents) {
+        fs.readFile('views/cats.html', 'utf8', function(errors, contents) {
             response.writeHead(200, {
                 'Content-type': 'text/html'
             });
@@ -32,13 +32,129 @@ var server = http.createServer(function(request, response) {
             response.end();
         });
     } else if (request.url === "/cars/new") {
-        fs.readFile('newCars.html', 'utf8', function(errors, contents) {
+        fs.readFile('views/newCars.html', 'utf8', function(errors, contents) {
             response.writeHead(200, {
                 'Content-type': 'text/html'
             });
             response.write(contents);
             response.end();
         });
+    } else if (request.url === '/images/bugatti-1.jpg') {
+        // notice we won't include the utf8 encoding
+        fs.readFile('./images/bugatti-1.jpg', function(errors, contents) {
+            response.writeHead(200, {
+                'Content-type': 'image/jpg'
+            });
+            response.write(contents);
+            response.end();
+        })
+    } else if (request.url === '/images/bugatti-2.jpg') {
+        // notice we won't include the utf8 encoding
+        fs.readFile('./images/bugatti-2.jpg', function(errors, contents) {
+            response.writeHead(200, {
+                'Content-type': 'image/jpg'
+            });
+            response.write(contents);
+            response.end();
+        })
+    } else if (request.url === '/images/bugatti-3.jpg') {
+        // notice we won't include the utf8 encoding
+        fs.readFile('./images/bugatti-3.jpg', function(errors, contents) {
+            response.writeHead(200, {
+                'Content-type': 'image/jpg'
+            });
+            response.write(contents);
+            response.end();
+        })
+    } else if (request.url === '/images/zonda-1.jpg') {
+        // notice we won't include the utf8 encoding
+        fs.readFile('./images/zonda-1.jpg', function(errors, contents) {
+            response.writeHead(200, {
+                'Content-type': 'image/jpg'
+            });
+            response.write(contents);
+            response.end();
+        })
+    } else if (request.url === '/images/zonda-2.jpg') {
+        // notice we won't include the utf8 encoding
+        fs.readFile('./images/zonda-2.jpg', function(errors, contents) {
+            response.writeHead(200, {
+                'Content-type': 'image/jpg'
+            });
+            response.write(contents);
+            response.end();
+        })
+    } else if (request.url === '/images/zonda-3.jpg') {
+        // notice we won't include the utf8 encoding
+        fs.readFile('./images/zonda-3.jpg', function(errors, contents) {
+            response.writeHead(200, {
+                'Content-type': 'image/jpg'
+            });
+            response.write(contents);
+            response.end();
+        })
+    } else if (request.url === '/images/lion-1.jpg') {
+        // notice we won't include the utf8 encoding
+        fs.readFile('./images/lion-1.jpg', function(errors, contents) {
+            response.writeHead(200, {
+                'Content-type': 'image/jpg'
+            });
+            response.write(contents);
+            response.end();
+        })
+    } else if (request.url === '/images/tiger-1.jpg') {
+        // notice we won't include the utf8 encoding
+        fs.readFile('./images/tiger-1.jpg', function(errors, contents) {
+            response.writeHead(200, {
+                'Content-type': 'image/jpg'
+            });
+            response.write(contents);
+            response.end();
+        })
+    } else if (request.url === '/images/cheetah-1.jpg') {
+        // notice we won't include the utf8 encoding
+        fs.readFile('./images/cheetah-1.jpg', function(errors, contents) {
+            response.writeHead(200, {
+                'Content-type': 'image/jpg'
+            });
+            response.write(contents);
+            response.end();
+        })
+    } else if (request.url === '/images/jaguar-1.jpg') {
+        // notice we won't include the utf8 encoding
+        fs.readFile('./images/jaguar-1.jpg', function(errors, contents) {
+            response.writeHead(200, {
+                'Content-type': 'image/jpg'
+            });
+            response.write(contents);
+            response.end();
+        })
+    } else if (request.url === '/images/mountain-1.jpg') {
+        // notice we won't include the utf8 encoding
+        fs.readFile('./images/mountain-1.jpg', function(errors, contents) {
+            response.writeHead(200, {
+                'Content-type': 'image/jpg'
+            });
+            response.write(contents);
+            response.end();
+        })
+    } else if (request.url === '/images/snow-leopard-1.jpg') {
+        // notice we won't include the utf8 encoding
+        fs.readFile('./images/snow-leopard-1.jpg', function(errors, contents) {
+            response.writeHead(200, {
+                'Content-type': 'image/jpg'
+            });
+            response.write(contents);
+            response.end();
+        })
+    } else if (request.url === '/stylesheets/style.css') {
+        fs.readFile('./stylesheets/style.css', 'utf8', function(errors, contents) {
+            response.writeHead(200, {
+                'Content-type': 'text/css'
+            });
+            response.write(contents);
+            response.end();
+        })
     }
     // request didn't match anything:
     else {
