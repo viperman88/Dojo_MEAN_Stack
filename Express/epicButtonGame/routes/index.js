e8c0bd8c3a1ca0043ca2873bc19e75e87a39cbcf
Robert Amato <robertamato@Roberts-MBP.fios-router.home>
2018-01-30 10:29:00 -0500

    updated syntax

--- Express/epicButtonGame/routes/index.js
+++ Express/epicButtonGame/routes/index.js
@@ -6,19 +6,17 @@ module.exports = function Route(app, server) {
     let count = 0;
     const io = require('socket.io').listen(server);
 
-    io.sockets.on('connection', function(socket) {
+    io.on('connection', function(socket) {
         console.log("Client/socket is connected!");
         console.log("Client/socket id is: ", socket.id);
         // all the server socket code goes in here
         socket.on("button_clicked", function() {
-            console.log("button clicked");
-            numberUpdated(count++);
-        });
+            numberUpdated(++count); // ++count alows it to update count prior to running numberUpdated
+        }); // this alows count to update with fresh information and not an initial older value
         socket.on("reset_count", function() {
-            console.log("RESET button clicked");
             numberUpdated(count = 0);
         });
-        socket.emit('update_counter', count);
+        socket.emit('update_counter', count); // updates new users with current info
 
     });
     // Number updated
