3f9a7d3c12a9ebad7948846e4d5443f8bd3a709c
Robert Amato <robertamato@Roberts-MBP.fios-router.home>
2018-01-31 16:50:13 -0500

    group chat

--- Express/groupChat/routes/index.js
+++ Express/groupChat/routes/index.js
@@ -14,7 +14,14 @@ module.exports = function Route(app, server) {
             socket.broadcast.emit('new_user', {
                 users: users
             });
+
         })
+
+        var temp_user = {};
+        temp_user[socket.id] = data.name;
+        socket.broadcast.emit('new_user', {
+            users: temp_user
+        });
 
         socket.on('new_message', function(data) {
             messages.push({
@@ -34,83 +41,3 @@ module.exports = function Route(app, server) {
         response.render('index')
     })
 }
-
-// Need to update user on current messages
-// console.log("new user", messages)
-// socket.emit('new_message', {
-//     messages: messages
-// });
-// Let user know their id.
-// socket.emit('user_id', {
-//     id: socket.id
-// });
-// Update all other users with just the new user.
-// var temp_user = {};
-// temp_user[socket.id] = data.name;
-// socket.broadcast.emit('new_user', {
-//     users: temp_user
-// });
-// socket.on("disconnect", function(data) {
-//     socket.broadcast.emit('remove_user', {
-//         id: socket.id
-//     });
-//     delete users[socket.id];
-// })
-// socket.on("send_message", function(data) {
-//     if (messages.length > max_messages) {
-//         // FIFO list. Take first on off the list.
-//         messages.shift();
-//     }
-// const users = [];
-//
-// const messages = [];
-//
-// function isUser(user) {
-//     const usersCount = users.length;
-//
-//     for (var i = 0; i < usersCount; i++) {
-//         if (user == users[i]) {
-//             return true;
-//         }
-//     }
-//     return false;
-// }
-// module.exports = function Route(app, server) {
-//     // this gets the socket.io module
-//     const io = require('socket.io').listen(server)
-//     //listen to connection even from the client side
-//     io.sockets.on("connection", function(socket) {
-//         console.log('Socket up')
-//         socket.on("page_load", function(data) {
-//             const existingUser = isUser(data.user);
-//             const event = existingUser ? 'existing_user' : 'load_messages';
-//             const errdata = existingUser ? {
-//                 error: "This user already exits"
-//             } : {
-//                 current_user: data.user,
-//                 messages: messages
-//             };
-//
-//             if (!existingUser) {
-//                 users.push(data.user);
-//             }
-//
-//             socket.emit(event, data);
-//         });
-//
-//         socket.on("new_message", function(data) {
-//             messages.push({
-//                 name: data.user,
-//                 message: data.message
-//             });
-//             io.emit("post_new_message", {
-//                 new_message: data.message,
-//                 user: data.user
-//             });
-//         });
-//     });
-//
-//     app.get("/", function(request, response) {
-//         response.render("index")
-//     });
-// };
