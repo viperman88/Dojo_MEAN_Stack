dfdc319876600aada3f335d382822831ef0cf0e3
Robert Amato <robertamato@Roberts-MBP.fios-router.home>
2018-02-16 10:11:22 -0500

    completed and styled

--- MongoDB/mongoose_dashboard/server.js
+++ MongoDB/mongoose_dashboard/server.js
@@ -1,25 +1,96 @@
 // Require modules
 const express = require('express');
-const body-parser = require('body-parser');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
 const port = process.env.PORT || 8000;
-
-// Create Express app
 const app = express();
 
-// Setting our Static and Views Folder Directory
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, './static')));
-app.set('views', path.join(__dirname, './views'));
-// Setting our View Engine to EJS
-app.set('view engine', 'ejs');
-// Integrate body-parser with our App
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
 
-app.get('/', function(req, res){
-    res.render('index')
+app.set('views', path.join(__dirname, './views'));
+app.set('view engine', 'ejs');
+
+mongoose.connect('mongodb://localhost/primates_db');
+mongoose.connection.on('connected', () => console.log('mongodb connected'));
+
+const primateSchema = new mongoose.Schema({
+    name: String,
+    color: String,
+    weight: Number,
+    origin: String
+})
+const Primate = mongoose.model('Primate', primateSchema)
+
+app.get('/primates', (req, res) => {
+    Primate.find({})
+        .then(primates => {
+            console.log(primates)
+            res.render('index', { primates: primates });
+        })
+        .catch(err => {
+            console.log('error caught', err);
+        });
 });
 
-app.listen(port, () => console.log(`express listening on port ${ port }`));
+app.get('/primates/new', (req, res) => {
+    res.render('new');
+});
+
+app.post('/primates', (req, res) => {
+    console.log('request body', req.body)
+    Primate.create(req.body)
+        .then(primate => {
+            console.log('created primate ', res);
+            res.redirect('/primates');
+        })
+        .catch(err => {
+            console.log('error creating primates', err);
+        })
+});
+
+app.get('/primates/:id', (req, res) => {
+    Primate.find({ _id: req.params.id })
+        .then(response => {
+            res.render('show', { primate: response[0] });
+        })
+        .catch(err => {
+            console.log(err);
+        });
+});
+
+app.get('/primates/:id/edit/', (req, res) => {
+    Primate.find({ _id: req.params.id })
+        .then(primate => {
+            console.log('editing: ', primate[0])
+            res.render('edit', { primate: primate[0] });
+        })
+        .catch(err => {
+            console.log('error editing primate', err);
+        });
+});
+
+app.post('/primates/:id', (req, res) => {
+    console.log(`Update primate id: ${req.params.id} and request body: ${req.body}`)
+    Primate.update({ _id: req.params.id }, req.body)
+        .then(primate => {
+            console.log('req:', req, 'res', res);
+            res.redirect('/primates');
+        })
+        .catch(err => {
+            console.log('error getting primate id', err);
+        })
+});
+
+app.post('/primates/:id/delete', function(req, res){
+    Primate.remove({ _id: req.params.id })
+        .then(primate => {
+            res.redirect('/primates');
+        })
+        .catch(err => {
+            console.log(err);
+        })
+});
+
+app.listen(port, () => console.log(`express listening on port ${port}`));
