70c82109e12ff42d79b6310e905945b90b07bd13
Robert Amato <robertamato@Roberts-MBP.fios-router.home>
2018-03-03 23:18:31 -0500

    Registration Form initial commit

---
+++ Angular/registrationForm/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // <-- Import FormsModule
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Registration Form';
+}
