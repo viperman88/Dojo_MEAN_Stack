c0df1d7fff4977db1df305b60ea676cd41e4dfd7
Robert Amato <robertamato@Roberts-MBP.fios-router.home>
2018-03-04 21:32:29 -0500

    Registration For working no error val yet

--- Angular/registrationForm/src/app/app.component.ts
+++ Angular/registrationForm/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms'; // <-- Import FormsModule
+import { User } from './user';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,11 @@ import { FormsModule } from '@angular/forms'; // <-- Import FormsModule
 })
 export class AppComponent {
   title = 'Registration Form';
+
+  user = new User();
+  users = [];
+  onSubmit() {
+    this.users.push(this.user);
+    this.user = new User();
+  }
 }
---
+++ Angular/registrationForm/src/app/user.ts
@@ -0,0 +1,12 @@
+export class User {
+  fName: string;
+  lName: number;
+  email: number;
+  password: string;
+  address: string;
+  unit: string;
+  city: string;
+  state: string;
+  lucky: string;
+  registered: boolean;
+}
